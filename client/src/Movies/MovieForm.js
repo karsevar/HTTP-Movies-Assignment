a1938a086075ff0dfbab983c3c481b4d1cb5e311
masonkarsevar <masonkarsevar@gmail.com>
2019-08-22 13:17:09 -0700

    update form is styled with semantic ui components. WIP: modifying the actors

--- client/src/Movies/MovieForm.js
+++ client/src/Movies/MovieForm.js
@@ -1,15 +1,31 @@
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
+import {Button, Form, Input} from 'semantic-ui-react';
+import styled from 'styled-components';
+
+const FormDiv = styled.div`
+    background-color: #fff;
+    border: 0;
+    box-shadow: 0 -1px 0 #e0e0e0, 0 0 2px rgba(0, 0, 0, 0.12),
+    0 2px 4px rgba(0, 0, 0, 0.24);
+    padding: 1rem;
+    cursor: pointer;
+    position: relative;
+    margin: 1rem auto;
+    max-width: 500px;
+`;
 
 function MovieForm(props) {
     console.log(props.match.params.id);
 
     const [updateMovie, setUpdateMovie] = useState({})
+    const [updateStars, setUpdateStars] = useState([]) 
 
     useEffect(() => {
         axios.get(`http://localhost:5000/api/movies/${props.match.params.id}`)
             .then(res => {
-                console.log(res.data)
+                // console.log(res.data)
+                setUpdateStars([...res.data.stars])
                 setUpdateMovie({...res.data})
             }) 
             .catch(err => console.log(err)) 
@@ -39,32 +55,56 @@ function MovieForm(props) {
         setUpdateMovie({...updateMovie, [event.target.name]: event.target.value})
     }
 
+    const handleStarsChange = (event, index) => {
+        const star = updateStars[index]
+        setUpdateStars([...updateStars, event.target.value])
+        console.log(updateStars)
+    }
+
 
     return (
-        <div className='movieform-container'>
-            <form onSubmit={updateSubmitMovie}>
-                <input 
-                    type='text'
-                    name='director'
-                    value={updateMovie.director}
-                    onChange={handleChange}
-                />
-                <input 
-                    type='text'
-                    name='metascore'
-                    value={updateMovie.metascore}
-                    onChange={handleChange}
-                />
-                <input
-                    type='text'
-                    name='title'
-                    value={updateMovie.title}
-                    onChange={handleChange}
-                />
-                <button>Submit Changes</button>
-            </form>
+        <FormDiv>
+            <Form onSubmit={updateSubmitMovie}>
+                <Form.Group widths='equal'>
+                    <Form.Input 
+                        type='text'
+                        name='director'
+                        value={updateMovie.director}
+                        onChange={handleChange}
+                    />
+                </Form.Group>
+                <Form.Group widths='equal'>
+                    <Form.Input 
+                        type='text'
+                        name='metascore'
+                        value={updateMovie.metascore}
+                        onChange={handleChange}
+                    />
+                </Form.Group>
+                <Form.Group widths='equal'>
+                    <Form.Input
+                        type='text'
+                        name='title'
+                        value={updateMovie.title}
+                        onChange={handleChange}
+                    />
+                </Form.Group>
+                {updateStars.map((star, index) => {
+                    return (
+                    <Form.Group key={index}  widths='equal'>
+                        <Form.Input
+                            type='text'
+                            name='star'
+                            value={updateStars[index]}
+                            //onChange={handleStarsChange}
+                        />
+                    </Form.Group>
+                    )
+                })}
+                <Button>Submit Changes</Button>
+            </Form>
 
-        </div>
+        </FormDiv>
     )
 }
 
