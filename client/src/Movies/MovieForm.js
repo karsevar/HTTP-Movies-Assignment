a6ec515d037bd95c813baf70df3e3ea017c112ea
masonkarsevar <masonkarsevar@gmail.com>
2019-08-22 08:51:34 -0700

    basic form finished. WIP: put functionality

--- client/src/Movies/MovieForm.js
+++ client/src/Movies/MovieForm.js
@@ -4,9 +4,50 @@ import axios from 'axios';
 function MovieForm(props) {
     console.log(props.match.params.id);
 
+    const [updateMovie, setUpdateMovie] = useState({})
+
+    useEffect(() => {
+        axios.get(`http://localhost:5000/api/movies/${props.match.params.id}`)
+            .then(res => {
+                console.log(res.data)
+                setUpdateMovie({...res.data})
+            }) 
+            .catch(err => console.log(err)) 
+    }, [])
+
+    const updateSubmitMovie = (event) => {
+        event.preventDefault();
+        console.log(updateMovie)
+    } 
+
+    const handleChange = event => {
+        setUpdateMovie({...updateMovie, [event.target.name]: event.target.value})
+    }
+
 
     return (
         <div className='movieform-container'>
+            <form onSubmit={updateSubmitMovie}>
+                <input 
+                    type='text'
+                    name='director'
+                    value={updateMovie.director}
+                    onChange={handleChange}
+                />
+                <input 
+                    type='text'
+                    name='metascore'
+                    value={updateMovie.metascore}
+                    onChange={handleChange}
+                />
+                <input
+                    type='text'
+                    name='title'
+                    value={updateMovie.title}
+                    onChange={handleChange}
+                />
+                <button>Submit Changes</button>
+            </form>
 
         </div>
     )
